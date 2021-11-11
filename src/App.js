77acdb1660062e3f0ca35de25e6239cf2d93a6e9
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-11-11 13:46:14 +0530

    routes added for stock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Routes, Route } from "react-router-dom";
+import { Stocks } from "./containers/Stocks";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/stocks">Stocks</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/stocks" element={<Stocks />}></Route>
+      </Routes>
     </div>
   );
 }
