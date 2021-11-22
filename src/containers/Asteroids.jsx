e5505d27aa93b0e96539eeb84390541b151bccda
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-11-22 10:25:58 +0530

    table created to list asteroids

--- src/containers/Asteroids.jsx
+++ src/containers/Asteroids.jsx
@@ -7,6 +7,19 @@ export const Asteroids = () => {
     <div>
       <h2>Asteroid Page</h2>
       <button>Click Me</button>
+      <div>
+        <h3>List of Asteroids Moving</h3>
+        <table>
+          <tbody>
+            <tr>
+              <th>REF ID</th>
+              <th>NAME</th>
+              <th>MAGNITUDE</th>
+              <th>JPL URL</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
