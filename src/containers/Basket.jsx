97abcde0d77848c3aeceb8f58a8fc64454534c9b
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-11-25 12:46:33 +0530

    product object with randomly generated properties

--- src/containers/Basket.jsx
+++ src/containers/Basket.jsx
@@ -1,4 +1,14 @@
 export const Basket = () => {
+  function getSampleProduct() {
+    console.log("product created");
+    return {
+      id: Math.floor(Math.random() * 10000000),
+      name: `Product Name ${Math.floor(Math.random() * 100000)}`,
+      quantity: Math.floor(Math.random() * 20 + 1),
+      price: Math.floor(Math.random() * 100 + 1),
+    };
+  }
+
   return (
     <div>
       <h2>Basket Component</h2>
