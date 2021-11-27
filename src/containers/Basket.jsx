6f1ef0d6d57b74dc9b7760b6ab3084f089801e56
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-11-27 11:56:01 +0530

    button for adding product via function

--- src/containers/Basket.jsx
+++ src/containers/Basket.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import { addProductToBasket } from "../redux/basket/action";
+
 export const Basket = () => {
   const [totalPrice, setTotalPrice] = useState(0);
   const storeData = useSelector((x) => x.products);
@@ -14,6 +16,15 @@ export const Basket = () => {
       quantity: Math.floor(Math.random() * 20 + 1),
       price: Math.floor(Math.random() * 100 + 1),
     };
+  }
+
+  function AddProductComponent() {
+    console.log("add DISPATCH called");
+    return (
+      <button onClick={() => dispatch(addProductToBasket(getSampleProduct()))}>
+        Add product to basket
+      </button>
+    );
   }
 
   return (
