f5398a094f52b3ac1b2c96829a27f680a98e0a97
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-12-07 12:22:09 +0530

    reducer for adding product

--- src/redux/basket/reducer.js
+++ src/redux/basket/reducer.js
@@ -4,6 +4,9 @@ const initialState = {
 
 export default function basketReducer(state = initialState, action) {
   switch (action.type) {
+    case "ADD_PRODUCT_TO_BASKET":
+      console.log("add REDUCER called...data received ---> ", action.payload);
+      return { ...state, products: [...state.products, action.payload] };
     default:
       console.log("default REDUCER called");
       return state;
