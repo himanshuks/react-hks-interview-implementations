aab017eedef6eefcb4399c46d6b4265eb350dc00
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-12-08 12:26:15 +0530

    reducer created for removing product

--- src/redux/basket/reducer.js
+++ src/redux/basket/reducer.js
@@ -7,6 +7,17 @@ export default function basketReducer(state = initialState, action) {
     case "ADD_PRODUCT_TO_BASKET":
       console.log("add REDUCER called...data received ---> ", action.payload);
       return { ...state, products: [...state.products, action.payload] };
+
+    case "REMOVE_PRODUCT_FROM_BASKET":
+      console.log(
+        "remove REDUCER called...data received ---> ",
+        action.payload
+      );
+      return {
+        ...state,
+        products: state.products.filter((x) => x.id !== action.payload),
+      };
+
     default:
       console.log("default REDUCER called");
       return state;
