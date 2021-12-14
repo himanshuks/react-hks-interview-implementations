a8dd615e3f8d06d54ebcf8a80aa67e4f85d9eb8c
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-12-14 10:43:58 +0530

    route links removed from App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { Link, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Stocks } from "./containers/Stocks";
 
 function App() {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/stocks">Stocks</Link>
-        </li>
-      </ul>
       <Routes>
         <Route path="/stocks" element={<Stocks />}></Route>
       </Routes>
