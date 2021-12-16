181507c2ede018fe19ff322922ba9669ebc36404
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-12-16 13:37:38 +0530

    nav bar added in project as header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import { NavbarMenu } from "./containers/NavbarMenu";
+
 import { Stocks } from "./containers/Stocks";
 
 function App() {
   return (
     <div>
+      <NavbarMenu />
       <Routes>
         <Route path="/stocks" element={<Stocks />}></Route>
       </Routes>
