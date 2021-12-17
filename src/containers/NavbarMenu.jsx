6588fb5e6305d88cccad45354175a1e572897f62
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-12-17 11:47:35 +0530

    using router link in bootstrap navlink

--- src/containers/NavbarMenu.jsx
+++ src/containers/NavbarMenu.jsx
@@ -1,12 +1,13 @@
 import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const NavbarMenu = () => {
   return (
     <div>
       <Navbar>
-        <Nav>
-          <Nav.Link href="/stocks">Stocks</Nav.Link>
-        </Nav>
+        <Nav.Link as={Link} to="/stocks">
+          Stocks
+        </Nav.Link>
       </Navbar>
     </div>
   );
