e28791512f7ae741c0471e886f96c31aab204408
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-12-23 10:05:24 +0530

    route for asteroids page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { NavbarMenu } from "./containers/NavbarMenu";
 
 import { Home } from "./containers/Home";
+import { Asteroids } from "./containers/Asteroids";
 import { Basket } from "./containers/Basket";
 import { Stocks } from "./containers/Stocks";
 
@@ -11,6 +12,7 @@ function App() {
       <NavbarMenu />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/asteroids" element={<Asteroids />}></Route>
         <Route path="/baskets" element={<Basket />}></Route>
         <Route path="/stocks" element={<Stocks />}></Route>
       </Routes>
