924ae20bae188c978f3c8af18f234081df625659
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-12-25 14:15:32 +0530

    comments added for bootstrap navbar

--- src/containers/NavbarMenu.jsx
+++ src/containers/NavbarMenu.jsx
@@ -1,6 +1,9 @@
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+// https://react-bootstrap.netlify.app/components/navbar/#navbar-api
+// Nav.Link as={Link} to="" ------ Used to use react route on NavLink instead of reloading page on click
+
 export const NavbarMenu = () => {
   return (
     <div>
