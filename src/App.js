81b78f59674382d3af4ce893253319075325c8c9
Himanshu Kumar Singh <52126577+himanshuks@users.noreply.github.com>
2022-04-12 14:24:10 +0530

    separate components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import { NavbarMenu } from "./containers/NavbarMenu";
+import { NavbarMenu } from "./components/NavbarMenu";
 
-import { Home } from "./containers/Home";
-import { Asteroids } from "./containers/Asteroids";
-import { Basket } from "./containers/Basket";
-import { Stocks } from "./containers/Stocks";
+import { Home } from "./components/home/Home";
+import { Asteroids } from "./components/asteroids/Asteroids";
+import { Basket } from "./components/baskets/Basket";
+import { Hooks } from "./components/hooks/Hooks";
+import { Stocks } from "./components/stocks/Stocks";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/asteroids" element={<Asteroids />}></Route>
         <Route path="/baskets" element={<Basket />}></Route>
+        <Route path="/hooks" element={<Hooks />}></Route>
         <Route path="/stocks" element={<Stocks />}></Route>
       </Routes>
     </div>
